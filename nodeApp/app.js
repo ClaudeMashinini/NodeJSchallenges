f70732b8951a54fde0df5eb600dddeca24206e02
Claude Mashinini <dumi.claude@gmail.com>
2019-05-10 18:51:44 +0200

    formFolder second task

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('static'));
+app.use(express.static('.'));
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, "./static"));
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 app.listen(8080);
